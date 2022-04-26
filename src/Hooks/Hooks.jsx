b2fd2b83952eb3f8e0edba32f41ff3b7579cba9c
JunaidShaikh2498 <junaidshaikh.241998@gmail.com>
2022-04-26 12:22:16 +0530

    3

--- src/Hooks/Hooks.jsx
+++ src/Hooks/Hooks.jsx
@@ -24,6 +24,12 @@ export default Hooks
 
 
 // useState() hook is used for declaring states in functional component as well as updating state
-// It takes 2 parameters : the state name and 
+// It takes 2 parameters : the state name and the setter function used to update state.
+// The state is updated on any event trigger on the browser. That event has a variable passed inside it which contains an
+// anonymous function. This function calls the setter function.
+// In case of Primitive data types like strings state values we directly pass the new state value in the set func.
+// But for non-primitive data type like object we have to consider a parameter of the previous state value.
+// Because if the state is set directly previous values will be lost so "..." operator is used to keep the 
+// previous values.
 
 
